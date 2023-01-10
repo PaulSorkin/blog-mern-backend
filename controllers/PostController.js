047a810103fdc34132fb09d4a97e232710a80c0c
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-01-10 23:03:01 +0200

    feat: delete post, update post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,4 +1,5 @@
 import PostModel from '../models/Post.js'
+import post from "../models/Post.js";
 
 export const getAll = async (req, res) => {
     try {
@@ -50,6 +51,40 @@ export const getOne = async (req, res) => {
     }
 }
 
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        PostModel.findOneAndDelete({
+            _id: postId
+        },
+            (err, doc) => {
+
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    message: 'Failed to delete the post',
+                });
+            }
+            if (!doc) {
+                console.log(err);
+                return res.status(404).json({
+                    message: 'The post has not been found',
+                });
+            }
+
+            res.json({
+                success: true,
+            });
+
+            });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to get the post',
+        });
+    }
+}
+
 export const create = async (req, res) => {
     try {
         const doc = new PostModel({
@@ -66,6 +101,31 @@ export const create = async (req, res) => {
         console.log(err);
         res.status(500).json({
             message: 'Failed to create the post',
+        });
+    }
+}
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        await PostModel.updateOne({
+            _id: postId,
+        }, {
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId,
+        });
+
+        res.json({
+            success: true,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to update the post',
         });
     }
 }

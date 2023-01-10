60852f4413a92a35f9e3df1d19119bb413eeb641
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-01-10 21:39:14 +0200

    feat: get one post and increment the post views counter

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -13,6 +13,43 @@ export const getAll = async (req, res) => {
     }
 }
 
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.findOneAndUpdate({
+            _id: postId,
+        }, {
+            $inc: {viewsCount: 1},
+        },
+            {
+                returnDocument: 'after',
+            },
+            (err, doc) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    message: 'Failed to get the post',
+                });
+            }
+
+            if (!doc) {
+                return res.status(404).json({
+                    message: 'The post is not found',
+                })
+            }
+
+            res.json(doc);
+
+            })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to get the post',
+        });
+    }
+}
+
 export const create = async (req, res) => {
     try {
         const doc = new PostModel({

c467e7b9fe86b16372347f25b3f4391cb9f4e695
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-01-12 22:56:49 +0200

    feat: add populate('user') to PostController.js

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -54,7 +54,8 @@ export const getOne = async (req, res) => {
 
             res.json(doc);
 
-            })
+            },
+            ).populate('user');
     } catch (err) {
         console.log(err);
         res.status(500).json({
